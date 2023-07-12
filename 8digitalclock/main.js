287366171ccce879026cb6ca46fcd988a2f2b2ae
Rohit <114095517+raizocoder@users.noreply.github.com>
2023-07-12 17:20:24 +0530

    Add files via upload

---
+++ 8digitalclock/main.js
@@ -0,0 +1,17 @@
+let time = document.getElementById("time");
+let format = document.getElementById("format");
+
+document.addEventListener("DOMContentLoaded", () => {
+  setInterval(showTime, 1000);
+});
+const showTime = () => {
+  let date = new Date();
+  let hr = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+  hr = hr < 10 ? `0${hr}` : hr;
+  min = min < 10 ? `0${min}` : min;
+  sec = sec < 10 ? `0${sec}` : sec;
+  time.innerHTML = `${hr}:${min}:${sec}`;
+  format.innerHTML = hr>=12? "PM":"AM"; 
+};
