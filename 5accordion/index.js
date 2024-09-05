83b74d74f8c7991cf231433789e61f978b5e0e74
Rohit Kumar <114095517+raizocoder@users.noreply.github.com>
2024-09-05 16:10:40 +0530

    Add files via upload

---
+++ 5accordion/index.js
@@ -0,0 +1,28 @@
+// var acc = document.getElementsByClassName("accordion");
+// var i;
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var panel = this.nextElementSibling;
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+//   });
+// }
+var acc = document.getElementsByClassName("accordion");
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    //   The scrollHeight property returns the height of element in pixels.
+    }
+  });
+}
